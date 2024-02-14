2315b0fd009139ab2812be5c8503fa3d517f52bf
Owen Garland <Ogarland9238@conestogac.on.ca>
2024-02-14 11:51:14 -0500

    authentication working just need to figure out the api gain

--- src/App.js
+++ src/App.js
@@ -1,14 +1,44 @@
+import { gapi } from "gapi-script";
 import { useEffect, useState } from "react";
 import "./index.css";
 import { jwtDecode } from "jwt-decode";
+import Event from "./components/Event";
+const SCOPE = "https://www.googleapis.com/auth/calendar";
 
 function App() {
   const [user, setUser] = useState({});
   const [isShown, setIsShown] = useState(false);
+  const [events, setEvents] = useState([]);
+  const [tokenClient, setTokenClient] = useState({});
+  const apiKey = process.env.REACT_APP_GOOGLE_CALENDAR_API_KEY;
 
   function toggle() {
     setIsShown((isShown) => !isShown);
   }
+
+  // const getEvents = (calendarID, apiKey) => {
+  //   function initiate() {
+  //     gapi.client
+  //       .init({
+  //         apiKey: apiKey,
+  //       })
+  //       .then(function () {
+  //         return gapi.client.request({
+  //           path: `https://www.googleapis.com/calendar/v3/calendars/awesomecreeper16@gmail.com/events`,
+  //         });
+  //       })
+  //       .then(
+  //         (response) => {
+  //           let events = response.result.items;
+  //           setEvents(events);
+  //         },
+  //         function (err) {
+  //           return [false, err];
+  //         }
+  //       );
+  //   }
+  //   gapi.load("client", initiate);
+  // };
 
   function handleCallbackResponse(response) {
     console.log("Encoded JWT ID token " + response.credential);
@@ -17,7 +47,13 @@ function App() {
     console.log(userObject);
 
     setUser(userObject);
+
+    // const calendarID = userObject.email;
+    // const events = getEvents(calendarID, apiKey);
+    // setEvents(events);
+
     toggle();
+
     document.getElementById("signInDiv").hidden = true;
   }
 
@@ -26,8 +62,12 @@ function App() {
     toggle();
     document.getElementById("signInDiv").hidden = false;
   }
+  function getCalendarEvents() {
+    tokenClient.requestAccessToken();
+  }
   useEffect(() => {
     /*global google */
+    const google = window.google;
     google.accounts.id.initialize({
       client_id: process.env.REACT_APP_CLIENT_ID,
       callback: handleCallbackResponse,
@@ -37,6 +77,39 @@ function App() {
       theme: "outline",
       size: "large",
     });
+
+    //Access tokens
+    //pull a users google calendar data.
+
+    setTokenClient(
+      google.accounts.oauth2.initTokenClient({
+        client_id: process.env.REACT_APP_CLIENT_ID,
+        scope: SCOPE,
+        callback: (tokenResponse) => {
+          console.log(tokenResponse);
+
+          //we now have access to a live token to use for any google API.
+          if (tokenResponse && tokenResponse.access_token) {
+            const calendarID = user.email;
+            fetch(
+              `https://www.googleapis.com/calendar/v3/calendars/${calendarID}/events`,
+              {
+                method: "GET",
+                headers: {
+                  "Content-Type": "application/json",
+                  Authorization: `Bearer ${tokenResponse.access_token}`,
+                },
+              }
+            )
+              .then((response) => response.json())
+              .then((data) => {
+                setEvents(data.events);
+              });
+          }
+        },
+      })
+    );
+    //tokenClient.requestAccessToken();
   }, []);
   //if we have no user: signin button
   // if we have a user:show the logout button
@@ -48,6 +121,18 @@ function App() {
           <img src={user.picture}></img>
           <h3>{user.name}</h3>
           <button onClick={(e) => handleSignOut(e)}>Sign Out</button>
+          <input
+            type="submit"
+            onClick={getCalendarEvents}
+            value="load Events"
+          />
+          <ul>
+            {events?.map((event) => (
+              <li key={event.id} className="flex justify-center">
+                <Event description={event.summary} />
+              </li>
+            ))}
+          </ul>
         </div>
       )}
     </div>
