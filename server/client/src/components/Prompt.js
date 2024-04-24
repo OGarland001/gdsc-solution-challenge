02de33d8494fcc26e833bf81a8139fa1e5f3aadf
Jackson Whynott <jmonkeyman15@gmail.com>
2024-04-24 12:21:57 -0400

    Added Keep/Discard Buttons

--- server/client/src/components/Prompt.js
+++ server/client/src/components/Prompt.js
@@ -2,6 +2,7 @@ import { Button } from "@material-tailwind/react";
 import React, { useState, useEffect } from "react";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
+import "./promptStyle.css";
 
 function Prompt({ eventList }) {
   const [selectedEvents, setSelectedEvents] = useState([]);
@@ -154,15 +155,19 @@ function Prompt({ eventList }) {
                 className="mb-2 w-full border border-gray-300 p-2 rounded-md text-black"
               />
             </div>
-            <div className="ml-4 flex flex-col justify-center">
-              <input
-                type="checkbox"
+            <div class="checkbox-wrapper-8 ml-4 flex flex-col justify-center">
+              <input 
+                type="checkbox" 
+                class="tgl tgl-skewed" 
                 id={`event-checkbox-${item.id}`}
                 checked={selectedEvents.includes(item.id)}
                 onChange={() => toggleEventSelection(item.id)}
               />
-              <label htmlFor={`event-checkbox-${item.id}`} className="ml-2">
-                Keep
+              <label 
+                for={`event-checkbox-${item.id}`} 
+                data-tg-on="Keep" 
+                data-tg-off="Discard" 
+                class="tgl-btn">
               </label>
             </div>
           </div>
