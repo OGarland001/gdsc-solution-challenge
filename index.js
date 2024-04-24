4a7648190713e8a4f92628618e72d2812d145584
Tyler Scheifley <tylerscheifley@gmail.com>
2024-04-24 10:42:35 -0400

    Added conference google meets json builder

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ const { JWT } = require("google-auth-library");
 const aiplatform = require("@google-cloud/aiplatform");
 const { PredictionServiceClient } = aiplatform.v1;
 const { helpers } = aiplatform;
+const { v4: uuidv4 } = require('uuid');
 
 app.use(
   cors({
@@ -212,6 +213,23 @@ function checkOpenDate(currentDates, jsonString) {
     }
   }
   return null;
+}
+
+function buildConferenceString()
+{
+  const requestId = uuidv4();
+
+  const conferenceData = {
+      createRequest: {
+          requestId: requestId,
+          conferenceSolutionKey: {
+              type: "hangoutsMeet",  
+          },
+      },
+  };
+
+  return conferenceData;
+
 }
 
 
